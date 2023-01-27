49e984eae7df91f2398de78c69bb75e88012e60f
Erin Timlin <erintimlin@gmail.com>
2023-01-27 18:55:56 -0500

    add UI Logic to html, and class to receipt in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//Business Logic-----
+
 //function Pizza(pizzaToppings, pizzaSize) {
 //this.pizzaSize = pizzaSize;
 //this.pizzeToppings = pizzaToppings;
@@ -17,17 +19,20 @@ Pizza.prototype.pizzaPrice = function(size) {
   } else {
       priceBySize = 10;
   }
+  return priceBySize;
 }
 
 
-Pizza.prototype.pizzaSize(orderSize) {
-  this.size[orderSize.size] = orderSize.id
+Pizza.prototype.orderSelections = function() {
+  return this.size + " " + this.toppings
 }
 
-Pizza.prototype.pizzaToppings = function(toppings) {
-}
+//Pizza.prototype.pizzaSize(orderSize) {
+//this.size[orderSize.size] = orderSize.id
+//}
 
-
+//Pizza.prototype.pizzaToppings = function(toppings) {
+//}
 
 
 ///cant get this to work
@@ -46,3 +51,21 @@ Pizza.prototype.pizzaToppings = function(toppings) {
     //  return this.priceBySize = 10;
 //  }
 //}
+
+//User Interface Logic
+const usersPizzaSize = document.querySelector("select#pizza-size-form").value;
+const usersPizzaToppings = document.querySelector("div#pizza-toppings").value;
+let newPizza = new Pizza(usersPizzaSize, usersPizzaToppings);
+
+function handleFormSubmission(){
+  event.preventDefault();
+  let usersReceipt = document.getElementById("usersOrder");
+  let usersSizeReceipt = document.getElementById("usersSize");
+  let usersToppingsReceipt = document.getElementById("usersToppings");
+  let usersPriceReceipt = document.getElementById("usersPrice");
+  usersOrder.removeAttribute("usersOrder");
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#pizza-form").addEventListener("submit", handleFormSubmission);
+});
