1c5d903c27c585467285d9bb5f6f347e13a44492
Erin Timlin <erintimlin@gmail.com>
2023-01-27 13:02:45 -0500

    add pizza pricing prototype

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,14 @@ function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 }
+
+Pizza.prototype.pizzaPrice = function(size) {
+  const priceBySize = 0
+    if (size === "large") {
+      return 20;
+  } else if (size === "medium") {
+      return 15;
+  } else {
+      return 10;
+  }
+}
