85ff8bb6b1d4c33b1e2c846609c3e7f4f368c851
Erin Timlin <erintimlin@gmail.com>
2023-02-02 10:15:57 -0500

    remove commented out code

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 //Business Logic-----
 
-
-
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
@@ -19,45 +17,15 @@ Pizza.prototype.pizzaPrice = function(size) {
 }
 
 
-
-
-
-//Pizza.prototype.pizzaSize(orderSize) {
-//this.size[orderSize.size] = orderSize.id
-//}
-
-//Pizza.prototype.pizzaToppings = function(toppings) {
-//}
-
-
-///cant get this to work
-//function Pizza(pizzaSize, pizzaToppings) {
-//this.pizzaSize = size;
-//this.pizzaToppings = toppings;
-//}
-
-//Pizza.prototype.pizzaPrice = function() {
-// const priceBySize = 0
-  //if (this.pizzaSize === "large") {
-    //return this.priceBySize = 20;
-  //} else if (this.pizzaSize === "medium") {
-    //  return this.priceBySize = 15;
- // } else {
-    //  return this.priceBySize = 10;
-//  }
-//}
-
 //User Interface Logic
-
 
 function handleFormSubmission(event){
   event.preventDefault();
   const usersPizzaSize = document.querySelector("select#pizza-size-form").value;
-  //const usersPizzaToppings = document.querySelector("div#pizza-toppings").value;
-  const usersPizzaToppings = document.querySelectorAll("input[name='toppings']:checked").value;
+  const usersPizzaToppings = document.querySelector("div#pizza-toppings").value;
+  //const usersPizzaToppings = document.querySelectorAll("input[name='topping']:checked"); 
   //const usersPizzaToppings = document.querySelectorAll("input[type=checkbox]:checked").value;
   let newPizza = new Pizza(usersPizzaSize, usersPizzaToppings);
-  //let toppings = newPizza.allToppings();
   let usersReceipt = document.getElementById("usersOrder");
   let usersSizeReceipt = document.getElementById("usersSize");
   let usersToppingsReceipt = document.getElementById("usersToppings");
