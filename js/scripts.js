c3c947935097ccf3f253ea0d70f0b1fadc31ceb4
Erin Timlin <erintimlin@gmail.com>
2023-02-07 10:44:36 -0500

    fix loop in UI and move commented out code to whiteboard

--- js/scripts.js
+++ js/scripts.js
@@ -16,57 +16,33 @@ Pizza.prototype.pizzaPrice = function(size) {
   return priceBySize;
 }
 
-// let inputTopping = 0;
-//   const pizzaToppings = document.getElemementById("pizza-toppings");
-//   for (let i=0; i < pizzaToppings.length; ++i) {
-//       if (pizzaToppings[i].checked) {
-//         inputTopping += parseInt(pizzaToppings[i].value);
-//       }
-//     }
-
-
-
 //User Interface Logic
 
 function handleFormSubmission(event){
   event.preventDefault();
     const usersPizzaSize = document.querySelector("select#pizza-size-form").value;
-    //const usersPizzaToppings = document.querySelector("div#pizza-toppings").value;
-    //const usersPizzaToppings = document.querySelectorAll("input[name='topping']:checked"); 
-    //const usersPizzaToppings = document.querySelectorAll("input[type=checkbox]:checked").value;
-    let inputTopping = 0;
-    const usersPizzaToppings = document.getElementsByClassName('pizza-toppings');
-    for (let i=0; i < usersPizzaToppings.length; ++i) {
-      if (usersPizzaToppings[i].checked) {
-        inputTopping += parseInt(usersPizzaToppings[i].value);
-      }
-    }
 
-    let newPizza = new Pizza(usersPizzaSize, usersPizzaToppings);
+    let toppingArr = [];
+    const usersPizzaToppings = document.querySelector("input[name='topping']:checked");
+    for (let i=0; i < toppingArr.length; i++) {
+      if (usersPizzaToppings[i].checked) {
+        toppingArr.push(usersPizzaToppings[i].id)
+        
+      }
+      return toppingArr;
+    };
+    
+    let newPizza = new Pizza(usersPizzaSize, toppingArr);
     let usersReceipt = document.getElementById("usersOrder");
     let usersSizeReceipt = document.getElementById("usersSize");
     let usersToppingsReceipt = document.getElementById("usersToppings");
     let usersPriceReceipt = document.getElementById("usersPrice");
     let receiptTotal = newPizza.pizzaPrice();
     usersSizeReceipt.innerText = newPizza.size;
-    usersToppingsReceipt.innerText = newPizza.toppings;
+    usersToppingsReceipt.innerText = newPizza.toppings.join(", ");
     usersPrice.innerText = receiptTotal;
     usersOrder.removeAttribute("class");
   }
-
-
-  // let inputTopping = [];
-  // let toppingList = document.querySelectorAll("input[type=checkbox]:checked");
-  // for (let i=0; i < toppingList.length; ++i) {
-  //   if (toppingList[i].checked) {
-  //     inputTopping += parseInt(toppingList[i].value);
-  //   }
-  // }
-
-
-  
-
-
 
 window.addEventListener("load", function() {
   document.getElementById("pizza-form").addEventListener("submit", handleFormSubmission);
